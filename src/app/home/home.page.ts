43bd9658fbed189cfc1ebabdeaab2d0d18ee21fc
Tamilyn <128110443+babimeaw@users.noreply.github.com>
2024-01-24 20:59:24 -0300

    Add files via upload

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  visor : string = '0' ;
+  valor1! : number;
+  valor2! : number;
+  operacao! : number;
+  
+  constructor(){}
+
+  addNum(valor : string){
+    if((this.visor.length == 1) && (this.visor == '0')){
+      this.visor = valor;
+    }else{
+      this.visor += valor;
+    }
+  }
+  
+  porcem(){
+    this.valor1 = +this.visor;
+    this.visor = "" + (this.valor1 /100);
+  }
+
+  inverso(){
+    this.valor1 = +this.visor;
+    this.visor = "" + (this.valor1 * -1)
+  }
+
+  ponto(){
+    this.valor1 = +this.visor;
+    this.visor = "" + (this.valor1 + ".")
+  }
+  calcular(){
+    this.valor2 = +this.visor;
+    switch(this.operacao){
+      case 0 : {
+        this.visor = "" + ( this.valor1 + this.valor2);
+        break;
+      }
+      case 1 : {
+        this.visor = "" + ( this.valor1 - this.valor2);
+        break;
+      }
+      case 2 : {
+        this.visor = "" + ( this.valor1 * this.valor2);
+        break;
+      }
+      case 3 : {
+      this.visor = "" + ( this.valor1 / this.valor2);
+      break;  
+      }
+    }
+  }
+
+  addOp(valor : number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    this.valor2 = Number(this.visor);
+    this.limpar();
+  }
+
+  limpar(){
+    this.visor = "0";
+  }
+
+}
